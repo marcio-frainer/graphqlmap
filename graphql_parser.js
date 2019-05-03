d166c7fd357297e12ab21dfb81378b707c869502
marcio-frainer <marciofrainer@hotmail.com>
2019-05-03 16:45:00 -0300

    Interpreter json file

--- graphql_parser.js
+++ graphql_parser.js
@@ -4,6 +4,20 @@
  * 
 */
 
-exports.GraphParser = function(jsonSchemmaStruct) {
-    
+var graph = require('graphql-tools');
+
+exports.GraphParser = function(jsonStruct, app) {
+    var route = jsonStruct.route;
+    var table = jsonStruct.table;
+    var mapping = jsonStruct.mapping[0];
+    var schemma = jsonStruct.schemma;
+
+    console.log(route);
+    app.get(route, (req, resp) => {
+        return resp.json({
+            msg: mapping.fieldName
+        })
+    })
+
+
 }
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+var graphql_parser = require('./graphql_parser');
+const map_parser = require('./mapping_parser');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const PORT = 5500;
+
+var graph = require('graphql-tools');
+
+
+//  "start": "nodemon index.js --exec babel-node --presets env,stage-2",
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 var folder = process
   .argv
@@ -7,8 +28,8 @@ var folder = process
 
 if (folder.length === 0) {
   console.log('Invalid path.');
-  return;
-};
+  exit;
+}
 
 var pathFile = folder[1];
 var extension = ".json";
@@ -18,42 +39,49 @@ function SetConsoleLog(text) {
   console.log(text);
 }
 
-LoadDirectory(pathFile);
-
-function LoadFile(file) {
-  try {
-    setConsoleLog("Loading: " + file + ".");
-    var readedFile = fs.readFileSync(file);
-    var jsonFile = JSON.parse(readedFile);
-  } catch (e) {
-    console.log(e);
-  }
-};
-
 function LoadDirectory(dir) {
   var files = fs.readdirSync(dir);
   files.forEach(function (x) {
-    var fileName = dir + x;
+    var fileName = ".\\" + dir + "\\" + x;
     if (fs.lstatSync(fileName).isDirectory()) {
       LoadDirectory(fileName);
       return;
     };
-    if (fs.lstatSync(filename).isFile()) {
-      var fileExt = path.extname(filename);
+    if (fs.lstatSync(fileName).isFile()) {
+      var fileExt = path.extname(fileName);
       if (fileExt != extension) 
         return;
-      LoadStructureFile(filename);
+      LoadStructureFile(fileName);
     };
   });
-};
+}
 
 function LoadStructureFile(jsonFile) {
-  if (!ValidateStructureFile(jsonFile)) {
-    SetConsoleLog(`The structure of file {jsonFile} is invalid!`);
-    return;
+  // if (!ValidateStructureFile(jsonFile)) {
+  //   SetConsoleLog("The structure of file " + jsonFile + " is invalid!");
+  //   return;
+  // }
+  LoadFile(jsonFile);
+}
+
+function LoadFile(file) {
+  try {
+    SetConsoleLog("Loading: " + file + ".");
+    var contents = fs.readFileSync(file);
+    var jsonContent = JSON.parse(contents);
+    console.log(jsonContent);
+    graphql_parser.GraphParser(jsonContent, app);
+  } catch (e) {
+    console.log(e);
   }
 }
 
 function ValidateStructureFile(jsonFile) {
 
 }
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`)
+})
+
+LoadDirectory(pathFile);
